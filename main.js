5c383fa73094d74dc809b7d7eeab634acdbc649b
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-09 22:48:37 -0300

    main.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 console.log('Leonardo Anselmo');
+
+var number = 0;
+
+console.log(typeof number);
+
+var number = "0";
+
+console.log(typeof number);
+
+function myFunction() {
+
+	var number = 0.1 + 1;
+
+	console.log(number);
+}
+
+myFunction();
