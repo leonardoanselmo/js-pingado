7ec7b8111e05a1379876967dd5bceac8cc772ad3
Leonardo Anselmo <leonardo_anselmo@hotmail.com>
2017-09-10 00:16:58 -0300

    samples

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
-console.log('Leonardo Anselmo');
+var prop1 = prompt("Modelo ?");
+var prop2 = prompt("Name ?");
+var prop3 = prompt("Color ?");
 
-var age = prompt("quantos anos você tem?");
+var obj = {
+    modelo: prop1,
+    name: prop2,
+    color: prop3
+};
 
-var ageParser = parseInt(age);
-
-if (ageParser >= 18) {
-	console.log('Você está autorizado.');
-} else {
-	console.log('Você não está autorizado.');
-}
+console.log(obj);
+console.log(JSON.stringify(obj));
 
 
+// function myFunction(name){
+// 	console.log('Nome => ', name);
+// }
+// 
+// myFunction("Leonardo Anselmo");
+
+//console.log('Leonardo Anselmo');
+
+// var age = prompt("quantos anos você tem?");
+
+//var ageParser = parseInt(age);
+
+//if (ageParser >= 18) {
+//	console.log('Você está autorizado.');
+//} else {
+//	console.log('Você não está autorizado.');
+//}
+
+
+
+//var i = 0;
+//while(i < 10){
+//	console.log(i);
+//	i++;
+//}
+
+//for (var i = 0; i < 10; i++) {
+//	console.log(i);
+//}
+
+//['Paulo', "Leonardo", "de", "Freitas", "Anselmo"].forEach(function(key, value){
+//	console.log(key, value);
+//});
 
 //var number = 0;
 
